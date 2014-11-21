1be6dda224233971199e890a33f2a37b7a9749b4
lyc <saber000@vip.qq.com>
2014-11-22 00:21:51 +0800

    initialize project structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# xcafeinfo
+a project created by LYC
+
+## Features
+
+* TODO
---
+++ xcafeinfo/cfgmgr.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from ConfigParser import ConfigParser
+from os.path import expanduser, dirname, realpath
+import sys
+
+
+class Sections(object):
+    """Sections of CONF
+    """
+    DEFAULT = "setting"
+    TEST = "test"
+
+
+CONF = ConfigParser()
+CONF.read("%s/xcafeinfo.ini" % dirname(sys.argv[0]))
+CONF.read(expanduser("~/conf/xcafeinfo.ini"))
+CONF.read(realpath("./xcafeinfo.ini"))
+
+# make sure all Sections.sections exists in CONF
+for s in dir(Sections):
+    if s.startswith("_"):
+        continue
+
+    section = getattr(Sections, s)
+    if not CONF.has_section(section) and section != "default":
+        CONF.add_section(section)
