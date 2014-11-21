4e9343c8792556199d05290ec423608334354121
lyc <saber000@vip.qq.com>
2014-11-22 02:22:28 +0800

    add a command to get xcafe info

--- xcafeinfo/xcafe.py
+++ xcafeinfo/xcafe.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import argparse
 from os import path
 from collections import namedtuple
 
@@ -37,3 +38,51 @@ class XCafe(object):
             port=pquery(s_port).val(),
             password=pquery(s_pass).val(),
             algorithm=pquery(s_alg).val())
+
+
+def parse_args(args, conf, sections):
+    parser = argparse.ArgumentParser(description="Get nodes info from xcafe")
+    parser.add_argument("path")
+    parser.add_argument(
+        "-e", "--endpoint", default=conf.get(sections.DEFAULT, "endpoint"))
+    parser.add_argument(
+        "-u", "--user", default=conf.get(sections.DEFAULT, "user"))
+    parser.add_argument(
+        "-p", "--password", default=conf.get(sections.DEFAULT, "password"))
+    parser.add_argument(
+        "-n", "--nodes", action="append", nargs='+',
+        default=conf.get(sections.DEFAULT, "nodes").split(","))
+
+    parser.add_argument(
+        "--server_selector", default=conf.get(sections.SELECTOR, "server"))
+    parser.add_argument(
+        "--port_selector", default=conf.get(sections.SELECTOR, "port"))
+    parser.add_argument(
+        "--password_selector", default=conf.get(sections.SELECTOR, "password"))
+    parser.add_argument(
+        "--algorithm_selector", default=conf.get(sections.SELECTOR, "algorithm"))
+
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    from xcafeinfo.cfgmgr import CONF, Sections
+    import sys
+
+    args = parse_args(sys.argv, CONF, Sections)
+
+    xcafe = XCafe(args.endpoint)
+    xcafe.login(args.user, args.password)
+    for node in args.nodes:
+        print "node:", node
+        nodeinfo = xcafe.node_info(
+            node,
+            s_server=args.server_selector,
+            s_port=args.port_selector,
+            s_pass=args.password_selector,
+            s_alg=args.algorithm_selector)
+
+        print "\tserver:", nodeinfo.server
+        print "\tport:", nodeinfo.port
+        print "\tpassword:", nodeinfo.password
+        print "\talgorithm:", nodeinfo.algorithm
